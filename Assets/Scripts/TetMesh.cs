f9db694e99f23cc00a258a43eb2f0e168447e6c1
Jung Nam <namxx054@umn.edu>
2016-12-06 09:48:19 -0600

    TetMesh Reader

--- Assets/Scripts/TetMesh.cs
+++ Assets/Scripts/TetMesh.cs
@@ -21,18 +21,24 @@ public class TetMesh : MonoBehaviour
 	public List<Vector4i> tetrahedra = new List<Vector4i> ();
 
     private Mesh mesh;
+    private MeshCollider collider;
 
     void Start ()
 	{
 		this.OpenFile (Application.dataPath + "/" + this.meshDirPath + "/" + this.filename);
-		Debug.Log ("- vertices: " + this.vertices.Count);
-		Debug.Log ("- tetrahedra: " + this.tetrahedra.Count);
+		Debug.Log ("- # of vertices: " + this.vertices.Count);
+		Debug.Log ("- # of tetrahedra: " + this.tetrahedra.Count);
+        Debug.Log("- # of tringles: " + this.triangles.Count);
+        Debug.Log("- # of surface triangles: " + this.surfaceTriangles.Count);
 
         // make an mesh (just from the surface triangles)
         Mesh mesh = new Mesh();
         this.GetComponent<MeshFilter>().mesh = mesh;
         this.mesh = mesh;
         this.UpdateMesh();
+
+        this.collider = this.GetComponent<MeshCollider>();
+        this.collider.sharedMesh = this.mesh;
     }
 
     /*void OnRenderObject()
@@ -71,9 +77,11 @@ public class TetMesh : MonoBehaviour
             newTriangles[t * 3 + 2] = tri[2];
         }
 
-        mesh.vertices = newVertices;
-        mesh.triangles = newTriangles;
-        mesh.RecalculateNormals();
+        this.mesh.vertices = newVertices;
+        this.mesh.triangles = newTriangles;
+
+        this.mesh.RecalculateBounds();
+        this.mesh.RecalculateNormals();
     }
     
     private void OpenFile (string filePath)
@@ -92,8 +100,8 @@ public class TetMesh : MonoBehaviour
 				Debug.LogError ("Error: failed to open file " + nodefile);
 				return;
 			}
-			// read the file.
-			using (FileStream file = new FileStream (nodefile, FileMode.Open))
+            // read the file.
+            using (FileStream file = new FileStream (nodefile, FileMode.Open))
 			using (StreamReader reader = new StreamReader (file)) {
 				string line = reader.ReadLine ();
 				string[] numbers = line.Split (new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
@@ -105,7 +113,7 @@ public class TetMesh : MonoBehaviour
 					Vector3 vert = new Vector3 (float.Parse (numbers [1]), float.Parse (numbers [2]), float.Parse (numbers [3]));
 					this.vertices.Add (vert);
 				}
-			}
+            }
 		} // end-nodes
         Debug.Log("- done reading the node file.");
 
@@ -115,8 +123,8 @@ public class TetMesh : MonoBehaviour
 				Debug.LogError ("Error: failed to open file " + elefile);
 				return;
 			}
-			// read the file.
-			using (FileStream file = new FileStream (elefile, FileMode.Open))
+            // read the file.
+            using (FileStream file = new FileStream (elefile, FileMode.Open))
 			using (StreamReader reader = new StreamReader (file)) {
 				string line = reader.ReadLine ();
 				string[] numbers = line.Split (new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
@@ -129,24 +137,25 @@ public class TetMesh : MonoBehaviour
 					this.tetrahedra.Add (tet);
 				}
 			}
-		} // end-tets
+        } // end-tets
         Debug.Log("- done reading the ele file.");
 
-        { // triangles
-			List<bool> isSurfaceTriangle = new List<bool> ();
+        { // triangles            
+            List<bool> isSurfaceTriangle = new List<bool> ();
 			for (int t = 0; t < this.tetrahedra.Count; t++) {
                 Vector4i tet = tetrahedra [t];
                 Vector3i[] tris = {
-					makeTriangle (tet [0], tet [2], tet [1]),
-					makeTriangle (tet [0], tet [1], tet [3]),
-					makeTriangle (tet [0], tet [3], tet [2]),
-					makeTriangle (tet [1], tet [2], tet [3])
+					MakeTriangle (tet [0], tet [2], tet [1]),
+                    MakeTriangle (tet [0], tet [1], tet [3]),
+                    MakeTriangle (tet [0], tet [3], tet [2]),
+                    MakeTriangle (tet [1], tet [2], tet [3])
 				};
 
 				for (int r = 0; r < 4; r++) {
                     Vector3i tri = tris [r];
                     Vector3i revTri = new Vector3i(tri[0], tri[2], tri[1]);
-					int revIndex = this.triangles.IndexOf (revTri);
+					//int revIndex = this.triangles.IndexOf (revTri); // too slow
+                    int revIndex = this.GetIndexOf(this.triangles, revTri); // much faster to use our own implementation.
 					if (revIndex == -1) { // not found
 						this.triangles.Add (tri);
 						isSurfaceTriangle.Add (true);
@@ -156,21 +165,35 @@ public class TetMesh : MonoBehaviour
 				}
 			}
 
-			for (int r = 0; r < this.triangles.Count; r++) {
+            for (int r = 0; r < this.triangles.Count; r++) {
 				if (isSurfaceTriangle [r]) {
 					this.surfaceTriangles.Add (triangles [r]);
 				}
 			}
-		} // end-triangles
+
+        } // end-triangles
         Debug.Log("- done making triangles.");
     }
 
+    // as described in https://www.dotnetperls.com/array-indexof
+    // it can be much fast to implement out own index of function.
+    private int GetIndexOf(List<Vector3i> list, Vector3i element)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            if ((list[i][0] == element[0]) && (list[i][1] == element[1]) && (list[i][2] == element[2]))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	private Vector3i makeTriangle (int i, int j, int k)
+
+	private Vector3i MakeTriangle (int i, int j, int k)
 	{
 		if (i < j && i < k) {
             return new Vector3i(i, j, k);
-
         } else if (j < i && j < k) {
             return new Vector3i(j, k, i);
         } else {
